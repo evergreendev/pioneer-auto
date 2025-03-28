ad20520f0daa03d9f8cee483f52c2f09f9f2c1e0
Joe simpson <joe@egmrc.com>
2025-03-28 10:22:50 -0600

    Enhance header layout and add operational hours section
    Refactored the header layout for better structure and visual hierarchy. Added a new section displaying today's operational hours with placeholder text for future dynamic updates. Adjusted logo dimensions and improved styling for consistency.

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -46,21 +46,32 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({
 
   return (
     <>
+      <div className="bg-brand-neutral-800 text-white">
+        <div className="container flex justify-between py-4 items-center">
+          <Link href="/" className={`w-32 transition-all ${centerNav ? 'hidden' : ''}`}>
+            <Image
+              src={logo.url || ''}
+              alt={logo.alt || 'Logo'}
+              width={200}
+              height={200}
+              priority
+              className="h-auto"
+            />
+          </Link>
+          <div>
+            <h2 className="font-display text-2xl">Today&#39;s Hours</h2>
+            <p className="font-bold">Everyday: 9am - 3pm</p>{/*todo change this to dynamic*/}
+            <p>(Weather permitting)</p>
+          </div>
+        </div>
+
+      </div>
+
       <header
         className={`hidden md:block sticky transition-colors top-0 z-50 ${!centerNav || hasScrolled ? 'bg-brand-neutral-700 text-white border-b border-blue-950' : ''}`}
         {...(theme ? { 'data-theme': theme } : {})}
       >
         <div className="container relative z-20 py-2 flex justify-between text-3xl ">
-          <Link href="/" className={`w-24 ${centerNav ? 'hidden' : ''}`}>
-            <Image
-              src={logo.url || ''}
-              alt={logo.alt || 'Logo'}
-              width={96}
-              height={48}
-              priority
-              className="h-auto"
-            />
-          </Link>
           <HeaderNav header={header} centerNav={centerNav} />
         </div>
       </header>
