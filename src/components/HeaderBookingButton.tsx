b080a2f10ff6acca9f5f3fac2f00189ef7f89177
Joe simpson <joe@egmrc.com>
2025-05-08 08:58:36 -0600

    Add HeaderBookingButton.tsx

---
+++ src/components/HeaderBookingButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HeaderBookingButton: React.FC = () => {
+  return (
+    <div>
+      {/* FareHarbor book button for item #631480 */}
+      <a 
+        href="https://fareharbor.com/embeds/book/pioneerautoshow/items/631480/?full-items=yes&flow=1393408"
+        className="inline-block px-4 py-2 bg-brand-accent-500 text-brand-neutral-800 font-bold text-base rounded hover:bg-brand-accent-600 transition-colors"
+      >
+        Book online now!
+      </a>
+    </div>
+  )
+}
+
+export default HeaderBookingButton
